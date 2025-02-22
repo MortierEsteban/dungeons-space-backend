d41329ca7af8d4796efb6d90aba226db30447632
MortierEsteban <81299235+MortierEsteban@users.noreply.github.com>
2025-02-22 05:08:23 +0100

    Functionning Docker-compose.yaml

--- node_service/cmd/node_service/main.go
+++ node_service/cmd/node_service/main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	database "github.com/MortierEsteban/dungeons-space-backend/node_service/internal/db"
+	"time"
 )
 
 func main() {
 	println(database.Dsn())
-	//time.Sleep(10 * time.Second)
+	println("Hello World")
+	for {
+		time.Sleep(10 * time.Second)
+		println("KeepAlive")
+	}
 }
--- node_service/internal/db/db.go
+++ node_service/internal/db/db.go
@@ -2,29 +2,26 @@ package database
 
 import (
 	"fmt"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
-	"log"
 	"os"
 )
 
-func DbConn() *gorm.DB {
-	db, err := gorm.Open(
-		postgres.Open(Dsn()), &gorm.Config{},
-	)
-	if err != nil {
-		log.Fatalf("There was error connecting to the database: %v", err)
-	}
-	return db
-}
+//func DbConn() *gorm.DB {
+//	db, err := gorm.Open(
+//		postgres.Open(Dsn()), &gorm.Config{},
+//	)
+//	if err != nil {
+//		log.Fatalf("There was error connecting to the database: %v", err)
+//	}
+//	return db
+//}
 
 func Dsn() string {
 	var (
-		username = os.Getenv("DB_USER")
-		password = os.Getenv("DB_PASSWORD")
-		name     = os.Getenv("DB_NAME")
-		host     = os.Getenv("DB_HOST")
-		port     = os.Getenv("DB_PORT")
+		username = os.Getenv("POSTGRES_USER")
+		password = os.Getenv("POSTGRES_PASSWORD")
+		name     = os.Getenv("POSTGRES_DB")
+		host     = os.Getenv("POSTGRES_HOST")
+		port     = os.Getenv("POSTGRES_PORT")
 	)
 	return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", username, password, host, port, name)
 }
