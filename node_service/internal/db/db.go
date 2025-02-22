5b49c8c1849802d446f87b77630e4764fdab0d0e
MortierEsteban <81299235+MortierEsteban@users.noreply.github.com>
2025-02-22 04:01:58 +0100

    Starting go setup

---
+++ node_service/internal/db/db.go
@@ -0,0 +1,30 @@
+package database
+
+import (
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+	"os"
+)
+
+func DbConn() *gorm.DB {
+	db, err := gorm.Open(
+		postgres.Open(dsn()), &gorm.Config{},
+	)
+	if err != nil {
+		log.Fatalf("There was error connecting to the database: %v", err)
+	}
+	return db
+}
+
+func Dsn() string {
+	var (
+		username = os.Getenv("DB_USER")
+		password = os.Getenv("DB_PASSWORD")
+		name     = os.Getenv("DB_NAME")
+		host     = os.Getenv("DB_HOST")
+		port     = os.Getenv("DB_PORT")
+	)
+	return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", username, password, host, port, name)
+}
